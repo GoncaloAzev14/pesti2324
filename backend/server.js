77ae8e0e8dc6a32329e787ff7a119c640653bafd
GoncaloAzev14 <1211560@isep.ipp.pt>
2024-05-17 12:37:23 +0100

    webRTC text chat added (not fully functional)

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,42 @@
-const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		methods: [ "GET", "POST" ]
-	}
-})
+  cors: {
+    origin: ["http://localhost:3000", "http://192.168.1.200:3000"],
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    // id
-	socket.emit("me", socket.id)
+  // Emit the ID to the newly connected client
+  socket.emit("me", socket.id);
 
-    // disconnect at some point --> call will end
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
+  // Broadcast when a user disconnects
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
 
-    // to call someone
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
+  // Handle calling a user
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
 
-    // answer a received call
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	})
-})
+  // Handle answering a call
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+  // Handle sending messages
+  socket.on("message", (data) => {
+    console.log(data);
+    io.to(data.to).emit("message", { from: data.from, text: data.text });
+  });
+});
+
+server.listen(5000, () => console.log("server is running on port 5000"));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,155 +9,232 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect("http://localhost:5000");
 function App() {
-	const [ me, setMe ] = useState("")
-	const [ stream, setStream ] = useState()
-	const [ receivingCall, setReceivingCall ] = useState(false)
-	const [ caller, setCaller ] = useState("")
-	const [ callerSignal, setCallerSignal ] = useState()
-	const [ callAccepted, setCallAccepted ] = useState(false)
-	const [ idToCall, setIdToCall ] = useState("")
-	const [ callEnded, setCallEnded] = useState(false)
-	const [ name, setName ] = useState("")
-	
-  const myVideo = useRef(null)
-	const userVideo = useRef()
-	const connectionRef= useRef()
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState([]);
 
-	useEffect(() => {
-		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
-			setStream(stream)
-			if (myVideo.current) {
-        myVideo.current.srcObject = stream;
-      }      
-		})
+  const myVideo = useRef(null);
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
-	socket.on("me", (id) => {
-			setMe(id)
-		})
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        if (myVideo.current) {
+          myVideo.current.srcObject = stream;
+        }
+      });
 
-		socket.on("callUser", (data) => {
-			setReceivingCall(true)
-			setCaller(data.from)
-			setName(data.name)
-			setCallerSignal(data.signal)
-		})
-	}, [])
+    socket.on("me", (id) => {
+      setMe(id);
+    });
 
-	const callUser = (id) => {
-		const peer = new Peer({
-			initiator: true,
-			trickle: false,
-			stream: stream
-		})
-		peer.on("signal", (data) => {
-			socket.emit("callUser", {
-				userToCall: id,
-				signalData: data,
-				from: me,
-				name: name
-			})
-		})
-		peer.on("stream", (stream) => {
-			
-				userVideo.current.srcObject = stream
-			
-		})
-		socket.on("callAccepted", (signal) => {
-			setCallAccepted(true)
-			peer.signal(signal)
-		})
+    socket.on("callUser", (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
 
-		connectionRef.current = peer
-	}
+    socket.on("message", (data) => {
+    //   if (data.from === idToCall || data.to === idToCall) {
+        setMessages([...messages, { text: data.text, sender: "other" }]);
+    //   }
+    });
+  }, [messages, idToCall]);
 
-	const answerCall =() =>  {
-		setCallAccepted(true)
-		const peer = new Peer({
-			initiator: false,
-			trickle: false,
-			stream: stream
-		})
-		peer.on("signal", (data) => {
-			socket.emit("answerCall", { signal: data, to: caller })
-		})
-		peer.on("stream", (stream) => {
-			userVideo.current.srcObject = stream
-		})
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
 
-		peer.signal(callerSignal)
-		connectionRef.current = peer
-	}
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name,
+      });
+    });
 
-	const leaveCall = () => {
-		setCallEnded(true)
-		connectionRef.current.destroy()
-	}
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
 
-	return (
-		<>
-			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
-		<div className="container">
-			<div className="video-container">
-				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-				</div>
-				<div className="video">
-					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-					null}
-				</div>
-			</div>
-			<div className="myId">
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
 
-				<TextField
-					id="filled-basic"
-					label="ID to call"
-					variant="filled"
-					value={idToCall}
-					onChange={(e) => setIdToCall(e.target.value)}
-				/>
-				<div className="call-button">
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
-						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{idToCall}
-				</div>
-			</div>
-			<div>
-				{receivingCall && !callAccepted ? (
-						<div className="caller">
-						<h1 >{name} is calling...</h1>
-						<Button variant="contained" color="primary" onClick={answerCall}>
-							Answer
-						</Button>
-					</div>
-				) : null}
-			</div>
-		</div>
-		</>
-	)
+    connectionRef.current = peer;
+  };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller });
+    });
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  };
+
+  const sendMessage = () => {
+    const messageInput = document.getElementById("message");
+    const message = messageInput.value;
+
+    if (message.trim() !== "") {
+      const recipientId = callAccepted ? caller : idToCall;
+      if (recipientId) {
+        socket.emit("message", { to: recipientId, from: me, text: message });
+      }
+      setMessages([...messages, { text: message, sender: me }]);
+      messageInput.value = "";
+    } else {
+      console.log("TENTASTE");
+    }
+  };
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", color: "#fff" }}>VIRTUAL AGENT</h1>
+      <div className="container">
+        <div className="video-container">
+          <div className="video">
+            {stream && (
+              <video
+                playsInline
+                muted
+                ref={myVideo}
+                autoPlay
+                style={{ width: "300px" }}
+              />
+            )}
+          </div>
+          <div className="video">
+            {callAccepted && !callEnded ? (
+              <video
+                playsInline
+                ref={userVideo}
+                autoPlay
+                style={{ width: "300px" }}
+              />
+            ) : null}
+          </div>
+        </div>
+        <div className="myId">
+          <TextField
+            id="filled-basic"
+            label="Name"
+            variant="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginBottom: "20px" }}
+          />
+          <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<AssignmentIcon fontSize="large" />}
+            >
+              Copy ID
+            </Button>
+          </CopyToClipboard>
+
+          <TextField
+            id="filled-basic"
+            label="ID to call"
+            variant="filled"
+            value={idToCall}
+            onChange={(e) => setIdToCall(e.target.value)}
+          />
+          <div className="call-button">
+            {callAccepted && !callEnded ? (
+              <Button variant="contained" color="secondary" onClick={leaveCall}>
+                End Call
+              </Button>
+            ) : (
+              <IconButton
+                color="primary"
+                aria-label="call"
+                onClick={() => callUser(idToCall)}
+              >
+                <PhoneIcon fontSize="large" />
+              </IconButton>
+            )}
+            {idToCall}
+          </div>
+        </div>
+        <div>
+          {receivingCall && !callAccepted ? (
+            <div className="caller">
+              <h1>{name} is calling...</h1>
+              <Button variant="contained" color="primary" onClick={answerCall}>
+                Answer
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        <div>
+          {callAccepted && !callEnded && (
+            <div>
+              <TextField id="message" variant="filled" />
+              <Button variant="contained" color="primary" onClick={sendMessage}>
+                Send
+              </Button>
+              <div>
+                {messages.map((message, index) => {
+                  const isSentMessage = message.sender === "me";
+                  const messageClass = isSentMessage
+                    ? "sent-message"
+                    : "received-message";
+                  const senderName = isSentMessage
+                    ? "Me"
+                    : name || "Unknown User";
+
+                  return (
+                    <div key={index} className={messageClass}>
+                      <span>{senderName}: </span>
+                      {message.text}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
