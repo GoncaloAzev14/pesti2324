6617d95790b496feab5e006e8f4bd5641a28a3cf
GoncaloAzev14 <1211560@isep.ipp.pt>
2024-06-19 03:24:34 +0100

    calling fixed

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -6,224 +6,35 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import PhoneOff from "@mui/icons-material/CallEnd";
 import NearMeIcon from "@mui/icons-material/Send";
 import PhoneDisabledIcon from "@mui/icons-material/PhoneDisabled";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import Peer from "simple-peer";
-import io from "socket.io-client";
 import "./Chat.css";
 
-const socket = io("https://socketiogoncalo.webpubsub.azure.com", {
-  path: "/clients/socketio/hubs/my_hub",
-});
-
-function Chat() {
-  const [me, setMe] = useState("");
-  const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [idToCall, setIdToCall] = useState("");
-  const [callEnded] = useState(false);
-  const [name, setName] = useState("");
-  const [otherName, setOtherName] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [isMuted, setIsMuted] = useState(false);
-  const [isCameraOff, setIsCameraOff] = useState(false);
-  const [calling, setCalling] = useState(false);
-
-  const myVideo = useRef(null);
-  const userVideo = useRef();
-  const connectionRef = useRef();
-
-  //===========================================================================================
-
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        setStream(stream);
-        if (myVideo.current) {
-          myVideo.current.srcObject = stream;
-        }
-      });
-
-    socket.on("me", (id) => {
-      setMe(id);
-    });
-
-    socket.on("callUser", (data) => {
-      setReceivingCall(true);
-      setCaller(data.from);
-      setCallerSignal(data.signal);
-      setOtherName(data.name);
-    });
-
-    // atualização das mensagens recebidas e enviadas
-    // referência ao backend socket.on("message", ...)
-    socket.on("message", (data) => {
-      setMessages([...messages, { text: data.text, sender: "other" }]);
-    });
-
-    socket.on("callDeclined", () => {
-      setCalling(false);
-      setReceivingCall(false);
-      alert("The call was declined.");
-    });
-
-    socket.on("callEnded", () => {
-      setCalling(false);
-      setIdToCall("");
-      setCallAccepted(false);
-      setReceivingCall(false);
-      setOtherName("");
-      setMessages([]);
-    });
-
-    return () => {
-      socket.off("me");
-      socket.off("callUser");
-      socket.off("message");
-      socket.off("callDeclined");
-      socket.off("callEnded");
-    };
-  }, [messages]);
-
-  //===========================================================================================
-
-  const callUser = (id) => {
-    setCalling(true);
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
-
-    peer.on("signal", (data) => {
-      socket.emit("callUser", {
-        userToCall: id,
-        signalData: data,
-        from: me,
-        name: name,
-      });
-    });
-
-    peer.on("stream", (stream) => {
-      if (userVideo.current) {
-        userVideo.current.srcObject = stream;
-      }
-    });
-
-    socket.on("callAccepted", (data) => {
-      setCalling(false);
-      setCallAccepted(true);
-      peer.signal(data.signal);
-      setOtherName(data.name);
-    });
-
-    connectionRef.current = peer;
-  };
-
-  //===========================================================================================
-
-  const answerCall = () => {
-    setCallAccepted(true);
-    setCalling(false);
-
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-
-    peer.on("signal", (data) => {
-      socket.emit("answerCall", { signal: data, to: caller, name: name });
-    });
-
-    peer.on("stream", (stream) => {
-      if (userVideo.current) {
-        userVideo.current.srcObject = stream;
-      }
-    });
-
-    peer.signal(callerSignal);
-    connectionRef.current = peer;
-  };
-
-  //===========================================================================================
-
-  const declineCall = () => {
-    setReceivingCall(false);
-    socket.emit("callDeclined", { to: caller });
-  };
-
-  //===========================================================================================
-
-  const leaveCall = () => {
-    socket.emit("endCall");
-    setIdToCall("");
-    setCallAccepted(false);
-    setReceivingCall(false);
-    setCalling(false);
-    setOtherName("");
-    setMessages([]);
-  };
-
-  //===========================================================================================
-
-  // método de envio de mensagens
-  // idToCall no caso de a mensagem ser enviada pelo user que fez a chamada
-  // caller no caso de a mensagem ser enviada pelo user que recebeu a chamada
-  // linha 211 para baixo fazem o tratamento das mensagens em html
-  const sendMessage = () => {
-    const messageInput = document.getElementById("message");
-    const message = messageInput.value;
-
-    if (message.trim() !== "") {
-      const recipientId = callAccepted ? caller : idToCall;
-
-      if (recipientId) {
-        socket.emit("message", { to: recipientId, from: me, text: message });
-      } else {
-        socket.emit("message", { to: idToCall, from: me, text: message });
-      }
-
-      setMessages([...messages, { text: message, sender: me }]);
-      messageInput.value = "";
-    } else {
-      console.log("Error Sending Messages");
-    }
-  };
-
-  //===========================================================================================
-
-  const toggleMute = () => {
-    const audioTrack = stream
-      .getAudioTracks()
-      .find((track) => track.kind === "audio");
-    if (audioTrack.enabled) {
-      audioTrack.enabled = false;
-      setIsMuted(true);
-    } else {
-      audioTrack.enabled = true;
-      setIsMuted(false);
-    }
-  };
-
-  //===========================================================================================
-
-  const toggleCamera = () => {
-    const videoTrack = stream
-      .getTracks()
-      .find((track) => track.kind === "video");
-    if (videoTrack.enabled) {
-      videoTrack.enabled = false;
-      setIsCameraOff(true);
-    } else {
-      videoTrack.enabled = true;
-      setIsCameraOff(false);
-    }
-  };
+const Chat = ({
+  myVideo,
+  userVideo,
+  me,
+  stream,
+  receivingCall,
+  callAccepted,
+  idToCall,
+  setIdToCall,
+  otherName,
+  calling,
+  callEnded,
+  name,
+  setName,
+  messages,
+  isMuted,
+  isCameraOff,
+  toggleMute,
+  toggleCamera,
+  callUser,
+  answerCall,
+  declineCall,
+  leaveCall,
+  sendMessage,
+ }) => {
 
   //===========================================================================================
 
